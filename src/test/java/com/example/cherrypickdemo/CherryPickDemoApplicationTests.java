7600724931458e1f5e91c68ecb59908136d0a8a8
홍선아 <144754093+HongSeonAh@users.noreply.github.com>
2025-03-12 11:47:56 +0900

    chore: 스프링 초기 세팅

---
+++ src/test/java/com/example/cherrypickdemo/CherryPickDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cherrypickdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CherryPickDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
